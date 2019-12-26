4b4f13e1778caf9c19fa7023eca7a6b6c82c7721
Larry-nan <lawrence,yinan@gmail.com>
2019-12-26 10:41:56 -0800

    html improvement

--- app/public/assets/friends.js
+++ app/public/assets/friends.js
@@ -1,23 +1,29 @@
 var dummy = [{
-        "name": "Mr 1s",
+        "name": "Ryan",
         "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
         "scores": [
-            1, 1, 1, 1, 1, 1, 1, 1, 1, 1
+            1, 1, 5, 1, 2, 1, 4, 1, 3, 1
         ]
     },
     {
-        "name": "Ms 5s",
+        "name": "Nick",
         "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
         "scores": [
-            5, 5, 5, 5, 5, 5, 5, 5, 5, 5
+            1, 3, 5, 5, 2, 5, 1, 5, 2, 5
         ]
     },
     {
-        "name": "mr 3s",
+        "name": "Link",
         "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
         "scores": [
-            3, 3, 3, 3, 3, 3, 3, 3, 3, 3
+            5, 3, 2, 3, 5, 3, 1, 3, 4, 3
         ]
     }
 ]
 module.exports = dummy
+
+// app.get('search/:key/:value', function(req,res){
+//     key= req.params.key
+//     value= req.params.value
+
+// })
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const dummydata = require('../public/assets/friends.js')
 const router = express.Router()
 const db = require('../public/assets/friends.js')
 router.get('/friends', (req, res) => {
@@ -8,32 +7,34 @@ router.get('/friends', (req, res) => {
 
 })
 router.post('/friends', (req, res) => {
-    let newUser = req.body
-    db.push(newUser)
-    res.send(req.body)
+    let newFriend = req.body
 
-})
-let newFriend = {
-    name: "newguy",
-    scores: []
-}
+    let bestMatch;
+    let closestScore = 100;
+    db.forEach(dummy => {
+        let dummydiff = 0
+        console.log(dummy.name)
+        dummy.scores.forEach((score, index) => {
+            let questiondiff = Math.abs(score - newFriend.scores[index])
+            console.log(questiondiff)
+            dummydiff += questiondiff
+        })
+        console.log(dummy.name, dummydiff)
+        if (dummydiff < closestScore) {
+            bestMatch = dummy.name;
+            closestScore = dummydiff
+        }
 
-let bestMatch;
-let closestScore = 100;
-dummydata.forEach(dummy => {
-    let dummydiff = 0
-    console.log(dummy.name)
-    dummy.scores.forEach((score, index) => {
-        let questiondiff = Math.abs(score - newFriend.scores[index])
-        console.log(questiondiff)
-        dummydiff += questiondiff
     })
-    console.log(dummy.name, dummydiff)
-    if (dummydiff < closestScore) {
-        bestMatch = dummy.name;
-        closestScore = dummydiff
-    }
-
+    console.log('before', db)
+    db.push(newFriend)
+    console.log('after', db)
+    console.log(`the best match is ${bestMatch} with a difference of ${closestScore}`)
+    res.send(bestMatch)
 })
-console.log(`the best match is ${bestMatch} with a difference of ${closestScore}`)
+// let newFriend = {
+//     name: "newguy",
+//     scores: [4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
+// }
+
 module.exports = router;
